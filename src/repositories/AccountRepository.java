1781b89fe1627c9ba99c00d441c4cb79a2facab3
gidskql6671 <gidskql6671@gmail.com>
2023-11-11 21:36:56 +0900

    회원 기능 구현

--- src/repositories/AccountRepository.java
+++ src/repositories/AccountRepository.java
@@ -37,4 +37,51 @@ public class AccountRepository {
 
 		return result;
 	}
+
+	public boolean changePassword(String studentId, String changedPassword) {
+		int rs = 0;
+		try {
+			String sql = "UPDATE STUDENT S SET S.LOGIN_PASSWORD = ? WHERE S.STUDENT_ID = ?";
+			PreparedStatement ps = conn.prepareStatement(sql);
+
+			ps.setString(1, changedPassword);
+			ps.setString(2, studentId);
+
+			rs = ps.executeUpdate();
+
+			conn.commit();
+
+			ps.close();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+
+		return rs == 1;
+	}
+
+	public double getGPA(String studentId) {
+		double result = 0;
+		try {
+			String sql = "SELECT AVG(GP.GRADE_POINT) AS GPA " +
+					"FROM STUDENT S, GRADE_POINT GP " +
+					"WHERE S.STUDENT_ID = GP.STUDENT_ID AND S.STUDENT_ID = ?";
+			PreparedStatement ps = conn.prepareStatement(sql);
+
+			ps.setString(1, studentId);
+
+			ResultSet rs = ps.executeQuery();
+
+			while(rs.next()) {
+				result = rs.getDouble(1);
+			}
+
+			rs.close();
+			ps.close();
+		}
+		catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+
+		return result;
+	}
 }
