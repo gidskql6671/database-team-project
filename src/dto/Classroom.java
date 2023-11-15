5ca2d5a8cb7d5ec4e47794ba11193d742973cb58
JM0015 <jiminnn51@gmail.com>
2023-11-15 14:02:43 +0900

    classroomService - 1. 건물 번호 목록 조회 기능 추가

---
+++ src/dto/Classroom.java
@@ -0,0 +1,22 @@
+package dto;
+
+public class Classroom {
+    public int buildingNumber;
+    public String roomCode;
+    public String name;
+
+    public Classroom(int buildingNumber, String roomCode, String name) {
+        this.buildingNumber = buildingNumber;
+        this.roomCode = roomCode;
+        this.name = name;
+    }
+
+    public Classroom(int buildingNumber) {
+        this(
+                buildingNumber,
+                "",
+                ""
+        );
+    }
+}
+
