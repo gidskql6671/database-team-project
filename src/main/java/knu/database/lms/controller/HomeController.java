5bc80f2d14a71f9a9a3e45a43c0538b40a2d265f
gidskql6671 <gidskql6671@gmail.com>
2023-11-20 23:17:35 +0900

    SQLException을 처리하는 GlobalExceptionHandler 추가

--- src/main/java/knu/database/lms/controller/HomeController.java
+++ src/main/java/knu/database/lms/controller/HomeController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.sql.SQLException;
 import java.util.List;
 
 @Controller
@@ -17,17 +18,18 @@ public class HomeController {
 	private final ClassroomRepository classroomRepository;
 
 	@GetMapping
-	public ModelAndView test(@SessionAttribute(name = "userId", required = false) Long userId) {
+	public void test(@SessionAttribute(name = "userId", required = false) Long userId) throws SQLException {
 		ModelAndView mav = new ModelAndView();
 
-		if (userId == null) {
-			mav.addObject("isLogin", false);
-		}
-		else {
-			mav.addObject("isLogin", true);
-		}
-		mav.setViewName("index");
-
-		return mav;
+		throw new SQLException("에러");
+//		if (userId == null) {
+//			mav.addObject("isLogin", false);
+//		}
+//		else {
+//			mav.addObject("isLogin", true);
+//		}
+//		mav.setViewName("index");
+//
+//		return mav;
 	}
 }
---
+++ src/main/java/knu/database/lms/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package knu.database.lms.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(SQLException.class)
+	public ResponseEntity<String> handleFileException() {
+		return ResponseEntity
+				.internalServerError()
+				.body("SQL 예외가 발생했습니다.");
+	}
+
+}
