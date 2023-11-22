ff87226392b9cff07cd7294c27a9440b4b62c867
gidskql6671 <gidskql6671@gmail.com>
2023-11-22 23:50:25 +0900

    불필요한 롬복 제거

--- src/main/java/knu/database/lms/dto/LoginRequestDto.java
+++ src/main/java/knu/database/lms/dto/LoginRequestDto.java
@@ -1,9 +1,7 @@
 package knu.database.lms.dto;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 @Getter
 public class LoginRequestDto {
 	private String loginId;
