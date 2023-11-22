b38f80167a8f7d52dc179804be39c2cd5d5ec33f
gidskql6671 <gidskql6671@gmail.com>
2023-11-22 23:59:03 +0900

    현재 열린 수업 조회, 수강 신청 구현

---
+++ src/main/java/knu/database/lms/controller/TakeClassController.java
@@ -0,0 +1,41 @@
+package knu.database.lms.controller;
+
+import knu.database.lms.dto.ClassInfo;
+import knu.database.lms.dto.TakeClassResult;
+import knu.database.lms.dto.controller.TakeClassRequestDto;
+import knu.database.lms.repositories.TakeClassRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.sql.SQLException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/sugang")
+@RequiredArgsConstructor
+public class TakeClassController {
+	private final TakeClassRepository takeClassRepository;
+
+	@GetMapping("/class")
+	public List<ClassInfo> getClasses() throws SQLException {
+		return takeClassRepository.getClasses(2023, "2");
+	}
+
+	@PostMapping
+	public void takeClass(
+			@RequestBody TakeClassRequestDto req,
+			@SessionAttribute(name = "userId", required = false) String userId
+			) throws SQLException {
+		if (userId == null) {
+			throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+		}
+
+		TakeClassResult result = takeClassRepository.takeClass(userId, req.getLectureCode(), req.getSectionCode());
+
+		if (!result.isSuccess) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, result.message);
+		}
+	}
+}
