717569564d6cadec7d2da85553eefbf7406cd74e
chaejm55 <chaejm55@gmail.com>
2023-11-22 15:36:22 +0900

    [Fix] 리뷰 사항 구현, 필요없는 DTO 삭제

--- src/main/java/knu/database/lms/controller/ClassController.java
+++ src/main/java/knu/database/lms/controller/ClassController.java
@@ -3,6 +3,7 @@ package knu.database.lms.controller;
 import knu.database.lms.dto.*;
 import knu.database.lms.repositories.ClassRepository;
 import lombok.RequiredArgsConstructor;
+import oracle.ucp.proxy.annotation.GetProxy;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -17,56 +18,65 @@ public class ClassController {
     private final ClassRepository classRepository;
 
     // 1. 현재 수강 중인 수업 목록 보기 (4번 쿼리)
-    @GetMapping("/list")
+    @GetMapping
     public List<ClassInfo> getClassList(@SessionAttribute(name = "userId", required = false) Long userId) throws SQLException {
        return classRepository.getTakingClass(userId.toString());
     }
 
     // 2. 수강 중인 수업의 게시글 목록 보기
-    @PostMapping("/post/list")
-    public List<Post> getClassPostList(@SessionAttribute(name = "userId", required = false) Long userId, @RequestBody PostRequestDto postRequestDto) throws SQLException {
-        isTakingClass(userId.toString(), postRequestDto.getLectureCode(), postRequestDto.getSectionCode());
-        return classRepository.getPosts(postRequestDto.getLectureCode(), postRequestDto.getSectionCode());
+    @GetMapping("/{lectureCode}/{sectionCode}/post")
+    public List<Post> getClassPostList(@SessionAttribute(name = "userId", required = false) Long userId,
+                                       @PathVariable String lectureCode, @PathVariable String sectionCode) throws SQLException {
+        isTakingClass(userId.toString(), lectureCode, sectionCode);
+        return classRepository.getPosts(lectureCode, sectionCode);
     }
 
     // 3. 수강 중인 수업의 게시글 보기
-    @PostMapping("/post")
-    public Post getClassPost(@SessionAttribute(name = "userId", required = false) Long userId, @RequestBody PostRequestDto postRequestDto) throws SQLException {
-        isTakingClass(userId.toString(), postRequestDto.getLectureCode(), postRequestDto.getSectionCode());
-        return classRepository.getPost(postRequestDto.getLectureCode(), postRequestDto.getSectionCode(), postRequestDto.getPostId());
+    @GetMapping("/{lectureCode}/{sectionCode}/post/{postId}")
+    public Post getClassPost(@SessionAttribute(name = "userId", required = false) Long userId,
+                             @PathVariable String lectureCode, @PathVariable String sectionCode,
+                             @PathVariable int postId) throws SQLException {
+        isTakingClass(userId.toString(), lectureCode, sectionCode);
+        return classRepository.getPost(lectureCode, sectionCode, postId);
     }
 
     // 4. 수강 중인 수업의 게시글의 댓글 목록 보기
-    @PostMapping("/post/comment/list")
-    public List<Comment> getCommentList(@SessionAttribute(name = "userId", required = false) Long userId, @RequestBody PostRequestDto postRequestDto) throws SQLException {
-        isTakingClass(userId.toString(), postRequestDto.getLectureCode(), postRequestDto.getSectionCode());
-        return classRepository.getComments(postRequestDto.getLectureCode(), postRequestDto.getSectionCode(), postRequestDto.getPostId());
+    @GetMapping("/{lectureCode}/{sectionCode}/post/{postId}/comment")
+    public List<Comment> getCommentList(@SessionAttribute(name = "userId", required = false) Long userId,
+                                        @PathVariable String lectureCode, @PathVariable String sectionCode,
+                                        @PathVariable int postId) throws SQLException {
+        isTakingClass(userId.toString(), lectureCode, sectionCode);
+        return classRepository.getComments(lectureCode, sectionCode, postId);
     }
 
     // 5. 어느 한 수업의 수강생 목록 보기
-    @PostMapping("/student/list")
-    public List<String> getTakingClassStudentList(@SessionAttribute(name = "userId", required = false) Long userId, @RequestBody ClassRequestDto classRequestDto) throws SQLException {
-        isTakingClass(userId.toString(), classRequestDto.getLectureCode(), classRequestDto.getSectionCode());
-        return classRepository.getStudents(classRequestDto.getLectureCode(), classRequestDto.getSectionCode());
+    @GetMapping("/{lectureCode}/{sectionCode}/students")
+    public List<String> getTakingClassStudentList(@SessionAttribute(name = "userId", required = false) Long userId,
+                                                  @PathVariable String lectureCode, @PathVariable String sectionCode) throws SQLException {
+        isTakingClass(userId.toString(), lectureCode, sectionCode);
+        return classRepository.getStudents(lectureCode, sectionCode);
     }
 
     // 6. 어느 한 수업의 수강생 수 보기 (5번 쿼리)
-    @PostMapping("/student/count")
-    public int getTakingClassStudentCount(@SessionAttribute(name = "userId", required = false) Long userId, @RequestBody ClassRequestDto classRequestDto) throws SQLException {
-        isTakingClass(userId.toString(), classRequestDto.getLectureCode(), classRequestDto.getSectionCode());
-        return classRepository.getStudentsCount(classRequestDto.getLectureCode(), classRequestDto.getSectionCode());
+    @GetMapping("/{lectureCode}/{sectionCode}/students/count")
+    public int getTakingClassStudentCount(@SessionAttribute(name = "userId", required = false) Long userId,
+                                          @PathVariable String lectureCode, @PathVariable String sectionCode) throws SQLException {
+        isTakingClass(userId.toString(), lectureCode, sectionCode);
+        return classRepository.getStudentsCount(lectureCode, sectionCode);
     }
 
     // 7. 특정 요일에 진행되는 강의 목록 보기 (12번 쿼리)
-    @PostMapping("/day/list")
-    public List<ClassInfo> getClassListByDayOfWeek(@SessionAttribute(name = "userId", required = false) Long userId, @RequestBody DayOfWeekRequestDto dayOfWeekRequestDto) throws SQLException {
-        return classRepository.getClassByDayOfWeek(dayOfWeekRequestDto.getDayOfWeek());
+    @GetMapping("/day")
+    public List<ClassInfo> getClassListByDayOfWeek(@SessionAttribute(name = "userId", required = false) Long userId, @RequestParam int dayOfWeek) throws SQLException {
+        return classRepository.getClassByDayOfWeek(dayOfWeek);
     }
 
     // 8. 수강 중인 수업의 게시글에 댓글 작성
-    @PostMapping("/post/comment")
-    public void writeComment(@SessionAttribute(name = "userId", required = false) Long userId, @RequestBody CreateCommentRequestDto commentRequestDto) throws SQLException {
-        isTakingClass(userId.toString(), commentRequestDto.getLectureCode(), commentRequestDto.getSectionCode());
+    @PostMapping("/{lectureCode}/{sectionCode}/post/{postId}/comment")
+    public void writeComment(@SessionAttribute(name = "userId", required = false) Long userId,
+                             @PathVariable String lectureCode, @PathVariable String sectionCode,
+                             @RequestBody CreateCommentRequestDto commentRequestDto) throws SQLException {
+        isTakingClass(userId.toString(), lectureCode, sectionCode);
         classRepository.writeComment(commentRequestDto.getPostId(), commentRequestDto.getContent(), userId.toString());
     }
 
