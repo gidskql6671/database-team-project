62eee5bc1161c21df7e208be5d338a3353a9dcb9
JM0015 <jiminnn51@gmail.com>
2023-11-23 19:07:05 +0900

    강의 목록 조회 페이지 제작

--- src/main/java/knu/database/lms/controller/HomeController.java
+++ src/main/java/knu/database/lms/controller/HomeController.java
@@ -1,16 +1,29 @@
 package knu.database.lms.controller;
 
 
+import knu.database.lms.dto.ClassInfo;
+import knu.database.lms.dto.Department;
+import knu.database.lms.repositories.DepartmentRepository;
+import knu.database.lms.repositories.TakeClassRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.sql.SQLException;
+import java.util.List;
 
 
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
+	private final TakeClassRepository takeClassRepository;
+	private final DepartmentRepository departmentRepository;
+
 
 	@GetMapping
 	public ModelAndView mainPage(@SessionAttribute(name = "userId", required = false) String userId) {
@@ -28,15 +41,33 @@ public class HomeController {
 	}
 
 	@GetMapping("/sugang")
-	public ModelAndView sugangPage(@SessionAttribute(name = "userId", required = false) String userId) {
+	public ModelAndView sugangPage(
+			@RequestParam(name = "departmentCode", required = false) String departmentCode,
+			@SessionAttribute(name = "userId", required = false) String userId
+	) throws SQLException {
 		ModelAndView mav = new ModelAndView();
 
 		if (userId == null) {
 			mav.setViewName("redirect:/");
+
+			return mav;
+		}
+		mav.setViewName("sugang");
+
+		List<ClassInfo> classInfos;
+		if (departmentCode == null) {
+			classInfos = takeClassRepository.getClasses(2023, "2");
 		}
 		else {
-			mav.setViewName("sugang");
+			Department department = departmentRepository.getDepartment(departmentCode);
+			if (department == null) {
+				throw new ResponseStatusException(HttpStatus.NOT_FOUND, "해당 학과가 존재하지 않습니다.");
+			}
+
+			classInfos = takeClassRepository.getClassesOf(departmentCode, 2023, "2");
 		}
+
+		mav.addObject("classInfos", classInfos);
 
 		return mav;
 	}
