1ea0f312a13e1c4c7ebc1966bbc91c996d0d2b2d
gidskql6671 <gidskql6671@gmail.com>
2023-11-28 21:59:42 +0900

    수업 상세보기 페이지 구현

--- src/main/java/knu/database/lms/dto/Post.java
+++ src/main/java/knu/database/lms/dto/Post.java
@@ -1,17 +1,21 @@
 package knu.database.lms.dto;
 
+import java.sql.Timestamp;
+
 public class Post {
 	public int id;
 	public String type;
 	public String title;
 	public String content;
 	public String publisherName;
+	public Timestamp createdAt;
 
-	public Post(int id, String type, String title, String content, String publisherName) {
+	public Post(int id, String type, String title, String content, String publisherName, Timestamp createdAt) {
 		this.id = id;
 		this.type = type;
 		this.title = title;
 		this.content = content;
 		this.publisherName = publisherName;
+		this.createdAt = createdAt;
 	}
 }
