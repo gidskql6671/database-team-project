2a61c69fefc7b94bed5bdae18c2b54c3fa978d10
chaejm55 <chaejm55@gmail.com>
2023-11-28 18:28:37 +0900

    특정 과 학생 목록 조회 구현 및 로그인 체크

--- src/main/java/knu/database/lms/controller/DepartmentController.java
+++ src/main/java/knu/database/lms/controller/DepartmentController.java
@@ -1,8 +1,14 @@
 package knu.database.lms.controller;
 
+import knu.database.lms.dto.Student;
 import knu.database.lms.repositories.DepartmentRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.sql.SQLException;
+import java.util.List;
 
 
 @RestController
@@ -11,5 +17,18 @@ import org.springframework.web.bind.annotation.*;
 public class DepartmentController {
     private final DepartmentRepository departmentRepository;
 
+    // 1. 특정 과의 학생 목록 조회(3번 쿼리)
+    @GetMapping("/{departmentCode}/student")
+    public List<Student> getStudentList(@SessionAttribute(name = "userId", required = false) String userId,
+                                        @PathVariable String departmentCode) throws SQLException {
+        isLogin(userId);
+        return departmentRepository.getStudents(departmentCode);
+    }
 
+
+    private void isLogin(String studentId) {
+        if (studentId == null) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "로그인이 필요합니다.");
+        }
+    }
 }
