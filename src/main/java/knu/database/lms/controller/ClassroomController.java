0f6d8f70bf18da082f5731df6cbded7c49f53215
chaejm55 <chaejm55@gmail.com>
2023-11-28 18:34:07 +0900

    누락된 세션 처리 및 로그인 확인 로직 추가

--- src/main/java/knu/database/lms/controller/ClassroomController.java
+++ src/main/java/knu/database/lms/controller/ClassroomController.java
@@ -3,10 +3,9 @@ package knu.database.lms.controller;
 import knu.database.lms.dto.Classroom;
 import knu.database.lms.repositories.ClassroomRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -19,13 +18,22 @@ public class ClassroomController {
 
     // 1. 건물 번호 목록 조회
     @GetMapping("/building")
-    public List<Classroom> getBuildingList() throws SQLException {
+    public List<Classroom> getBuildingList(@SessionAttribute(name = "userId", required = false) String userId) throws SQLException {
+        isLogin(userId);
         return classRoomRepository.getBuildings();
     }
 
     // 2. 특정 건물의 모든 강의실 목록 조회 (2번 쿼리)
     @GetMapping("/{buildingNumber}")
-    public List<Classroom> getClassroomList(@PathVariable int buildingNumber) throws SQLException {
+    public List<Classroom> getClassroomList(@SessionAttribute(name = "userId", required = false) String userId,
+                                            @PathVariable int buildingNumber) throws SQLException {
+        isLogin(userId);
         return classRoomRepository.getClassroomByBuilding(buildingNumber);
+    }
+
+    private void isLogin(String studentId) {
+        if (studentId == null) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "로그인이 필요합니다.");
+        }
     }
 }
