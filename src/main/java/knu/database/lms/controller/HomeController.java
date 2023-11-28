766f3d74176ac7576afa3dc87bbdf607dcdfc3d0
gidskql6671 <gidskql6671@gmail.com>
2023-11-29 00:20:13 +0900

    컨트롤러 연결

--- src/main/java/knu/database/lms/controller/HomeController.java
+++ src/main/java/knu/database/lms/controller/HomeController.java
@@ -26,4 +26,18 @@ public class HomeController {
 
 		return mav;
 	}
+
+	@GetMapping("/classroom")
+	public ModelAndView classroomPage(@SessionAttribute(name = "userId", required = false) String userId) {
+		ModelAndView mav = new ModelAndView();
+
+		if (userId == null) {
+			mav.setViewName("redirect:/");
+		}
+		else {
+			mav.setViewName("classroom");
+		}
+
+		return mav;
+	}
 }
