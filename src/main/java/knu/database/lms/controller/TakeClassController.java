e62515894a51a42defc360e020b06dd566337b7e
gidskql6671 <gidskql6671@gmail.com>
2023-11-28 23:17:15 +0900

    컨트롤러 구조 수정

--- src/main/java/knu/database/lms/controller/TakeClassController.java
+++ src/main/java/knu/database/lms/controller/TakeClassController.java
@@ -3,41 +3,60 @@ package knu.database.lms.controller;
 import knu.database.lms.dto.ClassInfo;
 import knu.database.lms.dto.Department;
 import knu.database.lms.dto.TakeClassResult;
-import knu.database.lms.dto.controller.GetClassesRequest;
 import knu.database.lms.dto.controller.TakeClassRequestDto;
 import knu.database.lms.repositories.DepartmentRepository;
 import knu.database.lms.repositories.TakeClassRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.sql.SQLException;
 import java.util.List;
 
-@RestController
-@RequestMapping("/api/sugang")
+@Controller
 @RequiredArgsConstructor
 public class TakeClassController {
 	private final TakeClassRepository takeClassRepository;
 	private final DepartmentRepository departmentRepository;
 
-	@GetMapping("/class")
-	public List<ClassInfo> getClasses(@RequestBody GetClassesRequest req) throws SQLException {
-		if (req.getDepartmentCode() == null) {
-			return takeClassRepository.getClasses(2023, "2");
+	@GetMapping("/sugang")
+	public ModelAndView sugangPage(
+			@RequestParam(name = "departmentCode", required = false) String departmentCode,
+			@SessionAttribute(name = "userId", required = false) String userId
+	) throws SQLException {
+		ModelAndView mav = new ModelAndView();
+
+		if (userId == null) {
+			mav.setViewName("redirect:/");
+
+			return mav;
+		}
+		mav.setViewName("sugang");
+
+		List<ClassInfo> classInfos;
+		if (departmentCode == null || departmentCode.equals("")) {
+			classInfos = takeClassRepository.getClasses(2023, "2");
 		}
 		else {
-			Department department = departmentRepository.getDepartment(req.getDepartmentCode());
+			departmentCode = departmentCode.toUpperCase();
+			Department department = departmentRepository.getDepartment(departmentCode);
 			if (department == null) {
 				throw new ResponseStatusException(HttpStatus.NOT_FOUND, "해당 학과가 존재하지 않습니다.");
 			}
 
-			return takeClassRepository.getClassesOf(req.getDepartmentCode(), 2023, "2");
+			classInfos = takeClassRepository.getClassesOf(departmentCode, 2023, "2");
 		}
+
+		mav.addObject("classInfos", classInfos);
+
+		return mav;
 	}
 
-	@PostMapping
+	@PostMapping("/api/sugang")
+	@ResponseBody
 	public void takeClass(
 			@RequestBody TakeClassRequestDto req,
 			@SessionAttribute(name = "userId", required = false) String userId
@@ -53,7 +72,8 @@ public class TakeClassController {
 		}
 	}
 
-	@DeleteMapping
+	@DeleteMapping("/api/sugang")
+	@ResponseBody
 	public void unTakeClass(
 			@RequestBody TakeClassRequestDto req,
 			@SessionAttribute(name = "userId", required = false) String userId
